61be90962f1ad2f6569a7b870410a15171e41136
B1229049 <B1229049@cgu.edu.tw>
2025-12-04 21:03:48 +0800

    define test1

--- webar-frontend/script.js
+++ webar-frontend/script.js
@@ -1,8 +1,8 @@
 // script.js - Web AR + 臉辨識
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-const SUPABASE_URL = "你的 supabase url";
-const SUPABASE_KEY = "你的 anon key";
+const SUPABASE_URL = "https://msuhvjhznkodpjfjpaia.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zdWh2amh6bmtvZHBqZmpwYWlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MzEwMTMsImV4cCI6MjA4MDQwNzAxM30.32jirKcLxE-sF3ICPD_yitBsO42JorbUgahz_1RAqoY";
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // HTML elements
