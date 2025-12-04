738f85dc600399a66b8bdb4c8fe2bc820629ede4
B1229049 <B1229049@cgu.edu.tw>
2025-12-05 02:03:38 +0800

    camera 3

--- webar-frontend/script.js
+++ webar-frontend/script.js
@@ -5,7 +5,7 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 // TODO：換成你的 anon key（不要用 service_role）
 const SUPABASE_URL = "https://msuhvjhznkodpjfjpaia.supabase.co";
-const SUPABASE_KEY = "你的 anon public key";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zdWh2amh6bmtvZHBqZmpwYWlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MzEwMTMsImV4cCI6MjA4MDQwNzAxM30.32jirKcLxE-sF3ICPD_yitBsO42JorbUgahz_1RAqoY";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
